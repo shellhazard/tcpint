f9e14a900cf78d006010aef8c62cda5ca451623a
unva1idated <unva1idated@protonmail.com>
2021-01-13 20:56:05 +1100

    Added injection methods

--- tcpint.go
+++ tcpint.go
@@ -20,6 +20,10 @@ type Proxy struct {
 	fromhandler func([]byte) []byte
 	tohandler   func([]byte) []byte
 	delimeter   byte
+
+	// dynamic fields
+
+	sync.Mutex
 }
 
 func NewProxy(from, to string, fromhandler, tohandler func([]byte) []byte, delimeter byte) *Proxy {
@@ -31,10 +35,28 @@ func NewProxy(from, to string, fromhandler, tohandler func([]byte) []byte, delim
 			"from": from,
 			"to":   to,
 		}),
-		fromhandler: fromhandler,
-		tohandler:   tohandler,
-		delimeter:   delimeter,
+		fromhandler:  fromhandler,
+		tohandler:    tohandler,
+		frominjector: []byte{},
+		toinjector:   []byte{},
+		delimeter:    delimeter,
 	}
+}
+
+func (p *Proxy) ToInject(b []byte) {
+	p.Lock()
+	defer p.Unlock()
+
+	r := append(p.toinjector, b...)
+	p.toinjector = r
+}
+
+func (p *Proxy) FromInject(b []byte) {
+	p.Lock()
+	defer p.Unlock()
+
+	r := append(p.frominjector, b...)
+	p.frominjector = r
 }
 
 // Start proxy server
@@ -93,13 +115,13 @@ func (p *Proxy) handle(connection net.Conn) {
 	wg := &sync.WaitGroup{}
 	wg.Add(2)
 	// Pushing data from client to remote host
-	go p.intercept(connection, remote, p.fromhandler, wg)
+	go p.intercept(connection, remote, p.fromhandler, *p.frominjector, wg)
 	// Pushing data to client from remote host
-	go p.intercept(remote, connection, p.tohandler, wg)
+	go p.intercept(remote, connection, p.tohandler, *p.toinjector, wg)
 	wg.Wait()
 }
 
-func (p *Proxy) intercept(from, to net.Conn, fn func([]byte) []byte, wg *sync.WaitGroup) {
+func (p *Proxy) intercept(from, to net.Conn, fn func([]byte) []byte, injector *[]byte, wg *sync.WaitGroup) {
 	defer wg.Done()
 	// Create reader
 	r := bufio.NewReader(from)
@@ -109,8 +131,18 @@ func (p *Proxy) intercept(from, to net.Conn, fn func([]byte) []byte, wg *sync.Wa
 		break
 	default:
 		for {
+			var buf []byte
+
+			// Read injected bytes
+			if len(injector) > 0 {
+				p.Lock()
+				_ = copy(buf, injector)
+				injector = nil
+				p.Unlock()
+			}
+
 			// Read bytes up to delimeter
-			buf, err := r.ReadBytes(p.delimeter)
+			buf, err = r.ReadBytes(p.delimeter)
 			if err != nil {
 				p.log.WithField("err", err).Errorln("Error from reader")
 				p.Stop()
